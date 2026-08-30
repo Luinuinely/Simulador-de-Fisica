bcec4addd398e625d9e870f65c97b73bab295049
Luiza <lui_poletto@hotmail.com>
2026-08-30 19:53:10 -0300

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+function qr(quant){
+    var cc=document.getElementById("cc");
+    if(quant=='1'){
+        cc.src="./src/img/cirquito_s1.png";
+    }
+    if(quant=='2'){
+        cc.src="./src/img/cirquito_s2.png";
+    }
+    if(quant=='3'){
+        cc.src="./src/img/cirquito_s3.png";
+    }
+}
+function askdfj(){
+    
+}
